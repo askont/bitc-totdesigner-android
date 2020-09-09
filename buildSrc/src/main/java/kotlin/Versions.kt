722eb5074f49d81c720fc46c4f10a54b5a4a176e
YWeber <yakowweberjavajob@gmail.com>
2020-09-09 15:43:50 +0200

    update version libs

--- buildSrc/src/main/java/kotlin/Versions.kt
+++ buildSrc/src/main/java/kotlin/Versions.kt
@@ -1,17 +1,17 @@
 object Versions {
     //Plugins
     const val android_gradle_plugin = "4.0.0"
-    const val kotlin_gradle_plugin = "1.3.7"
+    const val kotlin_gradle_plugin = "1.4.0"
     const val plugins_android_junit5 = "1.5.2.0"
     //AndroidX
     const val androidx_appcompat = "1.1.0"
-    const val androidx_constraint_layout = "2.0.0-beta4"
-    const val androidx_material = "1.1.0-alpha09"
+    const val androidx_constraint_layout = "2.0.0"
+    const val androidx_material = "1.2.1"
     const val androidx_cardview = "1.0.0"
-    const val androidx_recyclerview = "1.1.0-beta03"
-    const val ktx = "1.2.0-alpha03"
+    const val androidx_recyclerview = "1.1.0"
+    const val ktx = "1.3.1"
     const val multidex = "2.0.1"
-    const val fragment = "1.1.0-alpha09"
+    const val fragment = "1.2.5"
     const val flexbox_layout = "1.1.1"
     const val refresh_pull_layout = "1.0.0"
     const val transition_transition = "1.2.0"
@@ -23,7 +23,7 @@ object Versions {
     const val koin = "2.0.1"
 
     //Lifecycle
-    const val lifecycle = "2.2.0-alpha01"
+    const val lifecycle = "2.2.0"
 
     //conductor
     const val conductor_controller = "3.0.0-rc1"
